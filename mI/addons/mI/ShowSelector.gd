0adac824be37c9ad23dd273b170091fa42bfe7de
Damir Khakimov <damir.hakimov@gmail.com>
2023-03-23 15:36:52 +0400

    Added mI/addons/ShowSelector. Cool!

---
+++ mI/addons/mI/ShowSelector.gd
@@ -0,0 +1,29 @@
+extends Node2D
+export var Tag = ""
+
+onready var Variants = get_children()
+
+# Point is a Tag resolver and radio receiver
+var Point = load("res://addons/mI/point.gd")
+var point = Point.new()
+
+
+func _ready():
+	add_child(point)
+	for node in Variants:
+		node.visible = false
+	Directive('DIS')
+	pass # Replace with function body.
+
+
+func Radio(Value):
+	var variant = str(Value).to_upper()
+	print_debug('Selector got '+ Value)
+	for node in Variants:
+		node.visible = node.name.to_upper() == Value
+			
+#		node.visible = false
+
+func Directive(Value):
+	if Value == 'DIS':
+		Radio('DIS')
--- mI/addons/mI/mI_Indicator.gd
+++ mI/addons/mI/mI_Indicator.gd
@@ -4,39 +4,27 @@ export var ON_color =  Color(0.0, 1.0, 0.0, 1.0)
 export var OFF_color =  Color(1.0, 0.0, 0.0, 1.0)
 export var Undefined_color = Color(1.0, 1.0, 1.0, 0.5)
 var white = Color(1.0, 1.0, 1.0, 1.0)
-#export var Color_ON =  Color(1.0, 1.0, 1.0, 0.3)
-#export var Color_OFF =  Color(1.0, 1.0, 1.0, 0.3)
 
+# Point is a Tag resolver and radio receiver
+var Point = load("res://addons/mI/point.gd")
+var point = Point.new()
 
-onready var NW = get_node("/root/MiNetwork")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	NW.connect("Radio", self,  "Radio")
+	add_child(point)
+#	NW.connect("Radio", self,  "Radio")
 	self.antialiased = true
 	self.color = Undefined_color
 	pass # Replace with function body.
 
-func _enter_tree():  ## TODO!  ADD point child here!
-	if not Tag:
-		var P = self.get_parent()
-		while P:
-			if "Tag" in P:
-				Tag = P.Tag
-				break
-			else:
-				P = P.get_parent()
-	pass
-	#self.hint_tooltip = Tag
 
-
-func Radio(TagName, Value):
-	if TagName == Tag:
-		match Value:
-			"ON", 1:
-				self.color = ON_color
-			"OFF", 0:
-				self.color = OFF_color
-			_:
-				self.color = Undefined_color
+func Radio(Value):
+	match Value:
+		"ON", 1:
+			self.color = ON_color
+		"OFF", 0:
+			self.color = OFF_color
+		_:
+			self.color = Undefined_color
 
