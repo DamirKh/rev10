a19e30ffebcc2b846aab296796fa7691a73e204b
Damir Khakimov <damir.hakimov@gmail.com>
2023-03-30 09:41:57 +0400

    io.py fixed for ESP32-S3-WROOM-1

--- esp32/io.py
+++ esp32/io.py
@@ -7,8 +7,8 @@ from logic import switch_ladder, dallas, HeavyDutyPWM
 #from neopixel import NeoPixel
 
 # Switches
-G.DEVICES['SW_ON'] = switch_ladder.Switch_ladder(Pin(22, Pin.IN), inverted=True)
-G.DEVICES['SW_OFF'] = switch_ladder.Switch_ladder(Pin(19, Pin.IN), inverted=True)
+G.DEVICES['SW_ON'] = switch_ladder.Switch_ladder(Pin(5, Pin.IN), inverted=True)
+G.DEVICES['SW_OFF'] = switch_ladder.Switch_ladder(Pin(6, Pin.IN), inverted=True)
 
 # LEDS/RELAYS
 #G.DEVICES['LED1'] = Pin(23, Pin.OUT)
@@ -17,7 +17,7 @@ G.DEVICES['SW_OFF'] = switch_ladder.Switch_ladder(Pin(19, Pin.IN), inverted=True
 # pwm0 = PWM(Pin(23), freq=10, duty=0)         # create PWM object from a pin
 # G.DEVICES['PWM0'] = pwm0
 
-big_heater = HeavyDutyPWM(23, period=1)
+big_heater = HeavyDutyPWM(7, period=1)
 G.DEVICES['HEATER'] = big_heater
 
 
